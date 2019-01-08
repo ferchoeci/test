66c6ff0d489389e44876b41e3ef6338ea4b94895
Fernando Parra <fernando.parra@globallogic.com>
2019-01-08 15:23:29 -0300

    Test to circleCI

---
+++ src/main/java/com/tigoune/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,16 @@
+package com.tigoune.microservices.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients("com.tigoune.microservices.currencyconversionservice")
+@EnableDiscoveryClient
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+}
---
+++ src/main/java/com/tigoune/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,22 @@
+package com.tigoune.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+//@FeignClient(name="currency-exchange-service", url="localhost:8000") //se comenta para balancear por Ribbon
+//@FeignClient(name="currency-exchange-service")
+@FeignClient(name="netflix-zuul-api-gateway-server")
+@RibbonClient(name="currency-exchange-service")
+public interface CurrencyExchangeServiceProxy {
+
+		
+	//@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversionBean retreiveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
+	
+}
